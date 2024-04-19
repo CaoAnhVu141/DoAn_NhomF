8d27f9347dd9924d735fde46e27c856bac048e2a
PhanLongKhanh <phanlongkhanh.tdc2223@gmail.com>
2024-04-19 21:29:48 +0700

    commit lai

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminLoginAndRegisterController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
 
 //Show Login
 Route::get('login',[AdminLoginAndRegisterController::class,'ShowLogin']);
