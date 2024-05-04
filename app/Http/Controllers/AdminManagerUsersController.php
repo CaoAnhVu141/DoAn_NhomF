5d1b489de2b1565e9357b0639fd61c51164a4b8d
CaoAnhVu141 <caovu5541@gmail.com>
2024-05-04 18:22:00 +0700

    commit by caoanhvu

--- app/Http/Controllers/AdminManagerUsersController.php
+++ app/Http/Controllers/AdminManagerUsersController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdminMangerUserController extends Controller
+class AdminManagerUsersController extends Controller
 {
     //
-    //show giao diện quản lý users
 
     public function showManagerUsers()
     {
