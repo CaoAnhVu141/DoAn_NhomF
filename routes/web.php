ad03082b517c7daae6a85622982c126164e196b9
CaoAnhVu141 <161787991+CaoAnhVu141@users.noreply.github.com>
2024-05-11 17:02:07 +0700

    commit by caoanhvu

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\AdminShoppingController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AdminLoginAndRegisterController;
 use App\Http\Controllers\LoginAdminController;
+use App\Http\Controllers\SendMailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -169,3 +170,7 @@ Route::get('addproduct/addrun',[AdminProductTypeController::class,'AddNewProduct
 //__@Chi tiết users
 
 Route::get('manager',[AdminManagerUsersController::class,'showManagerUsers'])->name('manageruser');
+
+////demo senmail
+
+Route::get('sendmail',[SendMailController::class,'sendMaill']);
